ca1438a4a83928dc5e35e5f9e3a2e0e056563851
David-Aguilar2 <ab0255032025@unab.edu.sv>
2025-03-28 14:07:23 -0600

    Ejercicio de clase

---
+++ MetodosCadenas.py
@@ -0,0 +1,17 @@
+nombre="david alexander aguilar barrientos"
+
+#si es minúscula la primera letra en cada palabra, la vuelve mayúscula 
+print(nombre.title())
+
+#Texto en mayúscula 
+print(nombre.upper())
+
+#esta lo pasa a minúsculas nuevamente 
+print(nombre.upper().lower())
+
+#convierte solo la primera letra dela cadena en mayúscula
+print(nombre.capitalize())
+
+#Contar cuantas veces se repite la misma palabra 
+print(nombre.count("a"))
+
